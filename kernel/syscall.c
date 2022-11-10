028f5dfe1694a938ad5d86905d6caa88d161c5ab
JiangZhu-N <1160756683@qq.com>
2022-11-10 23:04:30 +0800

    trace

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -126,6 +126,31 @@ static uint64 (*syscalls[])(void) = {
     [SYS_trace] sys_trace,
 };
 
+static char *syscalls_name[] = {
+    [SYS_fork] "fork",
+    [SYS_exit] "exit",
+    [SYS_wait] "wait",
+    [SYS_pipe] "pipe",
+    [SYS_read] "read",
+    [SYS_kill] "kill",
+    [SYS_exec] "exec",
+    [SYS_fstat] "fstat",
+    [SYS_chdir] "chdir",
+    [SYS_dup] "dup",
+    [SYS_getpid] "getpid",
+    [SYS_sbrk] "sbrk",
+    [SYS_sleep] "sleep",
+    [SYS_uptime] "uptime",
+    [SYS_open] "open",
+    [SYS_write] "write",
+    [SYS_mknod] "mknod",
+    [SYS_unlink] "unlink",
+    [SYS_link] "link",
+    [SYS_mkdir] "mkdir",
+    [SYS_close] "close",
+    [SYS_trace] "trace",
+};
+
 void syscall(void)
 {
   int num;
@@ -140,7 +165,7 @@ void syscall(void)
 
     if ((1 << num) & p->trace_mask)
     {
-      printf("%d: syscall %s -> %d\n", p->pid, , p->trapframe->a0);
+      printf("%d: syscall %s -> %d\n", p->pid, syscalls_name[num], p->trapframe->a0);
     }
   }
   else
