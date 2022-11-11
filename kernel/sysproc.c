7f6d75ac67be0db9afc24305ff4424c1a094fe13
JiangZhu-N <1160756683@qq.com>
2022-11-11 19:03:03 +0800

    sysinfo

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -5,6 +5,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -98,5 +99,24 @@ uint64
 sys_trace(void)
 {
   argint(0, &(myproc()->trace_mask));
+  return 0;
+}
+
+uint64
+sys_sysinfo(void)
+{
+  struct sysinfo info;
+  freebytes(&info.freemem);
+  getprocnum(&info.nproc);
+
+  // 获取虚拟地址
+  uint64 dstaddr;
+  argaddr(0, &dstaddr);
+
+  // 从内核空间拷贝数据到用户空间
+  if (copyout(myproc()->pagetable, dstaddr, (char *)&info, sizeof(info)) < 0)
+  {
+    return -1;
+  }
   return 0;
 }
