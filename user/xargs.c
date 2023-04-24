f9188047b1bb3f24270c4b5637603c750a6c27c2
Alipebt <1160756683@qq.com>
2023-04-24 20:29:27 +0800

    util
    Signed-off-by: Alipebt <1160756683@qq.com>

--- user/xargs.c
+++ user/xargs.c
@@ -1,30 +1,35 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-
-#define MAXARGS 10
+#include "kernel/param.h"
 
 int main(int argc, char *argv[])
 {
-    int pid;
-    char *argv_new[MAXARGS];
-    char c, buf[512];
-    char *p = buf;
+    char *argv_new[MAXARG];
     int i;
+    char c, *p, buf[MAXARG];
 
+    // 将xargs后的参数全部传到新argv中
     for (i = 1; i < argc; i++)
     {
         argv_new[i - 1] = argv[i];
     }
 
-    while (read(0, &c, sizeof(char)))
+    p = buf;
+
+    // 每次读取一个字节，并记录到buf中
+    while (read(0, &c, sizeof(char)) > 0)
     {
         *p = c;
+        // 当遇到换行时结束此行，并执行exec
         if (c == '\n')
         {
+            // 将\n换为结束符
             *p = 0;
+            // buf也添加到新argv里
             argv_new[i - 1] = buf;
-            if ((pid = fork()))
+
+            if (fork())
             {
                 wait(0);
             }
